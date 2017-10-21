5d6937e7b0a5c4bc1e2a07c6401a0525fe02e3da
wesley jeanette <nicholas.jeanette@gmail.com>
2017-10-20 22:00:21 -0600

    longer leader key timeout

--- layouts/community/ergodox/wesley/config.h
+++ layouts/community/ergodox/wesley/config.h
@@ -7,7 +7,7 @@
 #define LSPO_KEY KC_9
 #define RSPC_KEY KC_0
 
-#define LEADER_TIMEOUT 800 // leader key sequence timeout in millis
+#define LEADER_TIMEOUT 1500 // leader key sequence timeout in millis
 
 #endif
 
